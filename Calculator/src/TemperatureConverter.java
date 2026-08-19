cfc1fb3b4a9f45e402aeae5728cd86ec3314b400
Yazratcharu <Polichris15@gmail.com>
2026-08-19 16:28:28 +0300

    switch practice

--- Calculator/src/TemperatureConverter.java
+++ Calculator/src/TemperatureConverter.java
@@ -1,17 +1,14 @@
 public class TemperatureConverter {
     public static void main(String[] args) {
         double celsius = 25.0;
-        double kalvins;
-        double forenheit;
-        char scale = 'C';
-        if ( scale == 'F'){
-            forenheit = celsius * 9/5 + 32;
-            System.out.println(celsius+"°C = "+forenheit+"°F");
-        } else if (scale == 'K') {
-            kalvins = celsius + 273.15;
-            System.out.println(celsius+"°C = "+kalvins+"°K");
-        } else {
-            System.out.println("Ошибка: Неизвестная шкала");
+        double kalvins = celsius + 273.15;
+        double forenheit = celsius * 9/5 + 32;
+        char scale = 'K';
+
+        switch (scale){
+            case 'F', 'f' -> System.out.println(celsius+"°C = "+forenheit+"°F");
+            case 'K','k' -> System.out.println(celsius+"°C = "+kalvins+"°K");
+            default -> System.out.println("Ошибка: Неизвестная шкала");
         }
 
     }
