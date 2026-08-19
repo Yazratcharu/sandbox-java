9edbbb2a36746f5d41e4fadcef1fff2d053e6f31
Yazratcharu <Polichris15@gmail.com>
2026-08-19 15:48:09 +0300

    Сделал консольный калькулятор

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,2 +1,30 @@
 public class Calculator {
-}
+
+    public static void main(String[] args) {
+        double a = 15.5;
+        double b = 11;
+        char operation = '*';
+        double result;
+
+        if (operation == '/' && b == 0) {
+            System.out.println("Ошибка деления на ноль");
+        } else if (operation == '/' ){
+                result = a/b;
+                System.out.println(a+" / "+b+" = "+result);
+            }else if (operation == '*'){
+                result = a*b;
+                System.out.println(a+" * "+b+" = "+result);
+            }else if (operation == '-'){
+                result = a-b;
+                System.out.println(a+" - "+b+" = "+result);
+            }else if(operation == '+'){
+                result = a+b;
+                System.out.println(a+" + "+b+" = "+result);
+            }else {
+                System.out.println("Неизвестная операция");
+            }
+        }
+
+    }
+
+
