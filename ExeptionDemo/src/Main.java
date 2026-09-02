d5f11fc7ee2436458aecb7eddd041a6c57e4f200
Yazratcharu <Polichris15@gmail.com>
2026-09-02 17:36:12 +0300

    Ошибки в демо

--- ExeptionDemo/src/Main.java
+++ ExeptionDemo/src/Main.java
@@ -1,14 +1,31 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
 
         try {
-            int[] massiv = {3,4,5};
-            System.out.println(massiv[4]);
+            int[] massiv = {51,3,32,4,5,14};
+            Scanner scan = new Scanner(System.in);
+            int index = scan.nextInt();
+            System.out.println("Введите индекс и я покажу вам число, которое скрывается под ним в массиве");
+            System.out.println("Ваш индекс: "+ index);
+            System.out.println("Число, которое скрывается под этим индексом: "+ massiv[index]);
         } catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("Упс, вышли за пределы массива");
-        } finally {
-            System.out.println("А этот finally выполнен ");
+            System.err.println("Упс, вышли за пределы массива, ошибка: " + e.getMessage());
+
+        }
+        catch (NullPointerException e){
+            System.err.println("Вы забыли инициализировать массив, не надо так, ошибка: " + e.getMessage());
+
+
+        }catch (InputMismatchException e){
+            System.err.println("В массиве только числа, так что нужно ввести целочисленное число, ошибка: " + e.getMessage());
+            e.printStackTrace();
+        }
+            finally {
+            System.out.println("Программа все еще работает и смогла завершиться! Удивительно! Вы плохо пытались ее сломать, ну или даже не пытались");
         }
 
     }
